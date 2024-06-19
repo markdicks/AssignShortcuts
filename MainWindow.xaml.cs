9c34cf6ed8849d941621801a1319a06c5294e50f
markdicks <markdicks03@gmail.com>
2024-06-19 10:38:21 +0200

    Added event handler stubs for device selection and shortcut management actions. Added SetWindowProperties function

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace AssignShortcuts
 {
@@ -19,6 +10,40 @@ namespace AssignShortcuts
         public MainWindow()
         {
             InitializeComponent();
+            SetWindowProperties();
+        }
+
+        // Sets the window size and position
+        private void SetWindowProperties()
+        {
+            this.Width = 800;
+            this.Height = 600;
+
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        // Event handler for device selection change
+        private void DeviceListBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            // Logic to handle device selection change
+        }
+
+        // Event handler for adding a shortcut
+        private void AddShortcut_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic to handle adding a shortcut
+        }
+
+        // Event handler for removing a shortcut
+        private void RemoveShortcut_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic to handle removing a shortcut
+        }
+
+        // Event handler for updating a shortcut
+        private void UpdateShortcut_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic to handle updating a shortcut
         }
     }
 }
